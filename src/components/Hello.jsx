170e404bc955e8f6dfa1811f4f4b557e14bf95d7
alejandra <alejandra.barreto.ab@gmail.com>
2021-02-20 20:08:06 -0500

    first commit

---
+++ src/components/Hello.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const Hi = () => (
+    <h1>Hey people</h1>
+);
+
+export default Hi;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hi from './components/Hello';
+
+ReactDOM.render(<Hi />, document.getElementById('app'));
